e634a65c29608e4c3311bf2d9b5f6d57f7d0a144
hzucmj <hzucmj@gmail.com>
2012-02-08 22:07:41 +0800

    update MainPanel

--- app/view/MainPanel.js
+++ app/view/MainPanel.js
@@ -6,17 +6,8 @@ Ext.define('App.view.MainPanel', {
 		name: 'elbs-mainpanel',
 		tabBarPosition: 'bottom',
 	
-		//defaults: {
-			//scrollable: true
-		//}
-		/*,
-
-		items: [
-			Ext.create('app.view.MsgList'),
-			Ext.create('app.view.SearchPanel'), 
-			Ext.create('app.view.InfoPanel'),
-			Ext.create('app.view.UserPanel'),
-			Ext.create('app.view.MorePanel')
-		]*/
+		defaults: {
+			scrollable: true
+		}
 	}	
 });
